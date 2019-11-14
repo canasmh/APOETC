3f37bea692507ccbf9629f8209bfa84cec469423
canasmh <55603689+canasmh@users.noreply.github.com>
2019-11-14 03:28:26 -0700

    Added a short description and author info.

--- setup.py
+++ setup.py
@@ -3,10 +3,20 @@
 from setuptools import setup
 
 setup(name='APOETC',
-      version='1.0',
+      version='0.1.0',
+      author='Manuel H. Canas, Alexander Stone-Martinex, Bryson Stemock, Rogelio Ochoa, Hasan Rahman'
+      author_email = 'canasmh@nmsu.edu'
       packages=['APOETC'],
       scripts=['./bin/etc_script.py'],
       include_package_data=True,
       package_data={'APOETC':['../data/Sky/*.txt','../data/APO/Arctic/*.dat']}
+      description='An exposure time calculator for the ARC 3.5m telescope'
+      install_requires = [
+        "numpy",
+        "synphot",
+        "astropy",
+        "matplotlib",
+        "scipy"
+        ]
       )
 
