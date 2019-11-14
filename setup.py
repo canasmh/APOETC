a79e7b1826d1277650abe5ab94cdb240fa68326a
canasmh <55603689+canasmh@users.noreply.github.com>
2019-11-14 14:55:28 -0700

    minor changes to data package

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='APOETC',
       packages=['APOETC'],
       scripts=['./bin/etc_script.py'],
       include_package_data=True,
-      package_data={'APOETC':['../data/Sky/*.txt','../data/APO/Arctic/*.dat']},
+      package_data={'APOETC':['./../data/Sky/*.txt','./../data/APO/Arctic/*.dat']},
       description='An exposure time calculator for the ARC 3.5m telescope'
       )
 
